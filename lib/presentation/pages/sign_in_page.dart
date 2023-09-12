ea968d8dc7b93867369317acfa70184cb0a6979b
maguroumai <syabusyabu.umai@gmail.com>
2023-09-12 19:11:00 +0900

    sign in email

--- lib/presentation/pages/sign_in_page.dart
+++ lib/presentation/pages/sign_in_page.dart
@@ -1,8 +1,15 @@
+import 'dart:async';
+
+import 'package:adaptive_dialog/adaptive_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:sign_in_button/sign_in_button.dart';
 import 'package:untitled_2/model/use_cases/auth/sign_in_with_account.dart';
 import 'package:untitled_2/model/use_cases/auth/sign_in_with_anonymously.dart';
+import 'package:untitled_2/model/use_cases/auth/sign_in_with_email_and_password.dart';
+import 'package:untitled_2/presentation/custom_hooks/use_global_key.dart';
+import 'package:untitled_2/presentation/widgets/show_indicator.dart';
+import 'package:untitled_2/utils/logger.dart';
 
 import '../../model/repositories/firebase_auth/login_type.dart';
 
@@ -11,6 +18,9 @@ class SingInPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final emailFormFieldKey = useFormFieldStateKey();
+    final passwordFormFieldKey = useFormFieldStateKey();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('ログイン'),
@@ -21,6 +31,69 @@ class SingInPage extends HookConsumerWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              TextFormField(
+                key: emailFormFieldKey,
+                decoration: const InputDecoration(labelText: 'メールアドレス'),
+                validator: (value) => (value == null || value.isEmpty)
+                    ? '正しいメールアドレスを入力してください'
+                    : null,
+              ),
+              TextFormField(
+                key: passwordFormFieldKey,
+                decoration: const InputDecoration(
+                  labelText: 'パスワード',
+                  hintText: '大文字小文字含む英数字8桁以上',
+                ),
+                validator: (value) =>
+                    (value == null || value.isEmpty) ? 'パスワード' : null,
+                obscureText: true,
+              ),
+              ElevatedButton(
+                child: const Text('ログイン'),
+                onPressed: () async {
+                  final isValidEmail =
+                      emailFormFieldKey.currentState?.validate() ?? false;
+                  final isValidPassword =
+                      passwordFormFieldKey.currentState?.validate() ?? false;
+                  false;
+                  if (!isValidEmail || !isValidPassword) {
+                    logger.info('Invalid input data');
+                    return;
+                  }
+
+                  final email = emailFormFieldKey.currentState?.value;
+                  final password = passwordFormFieldKey.currentState?.value;
+                  if (email == null || password == null) {
+                    return;
+                  }
+
+                  try {
+                    showIndicator(context);
+
+                    await ref.read(signInWithEmailAndPasswordProvider)(
+                      email: email,
+                      password: password,
+                    );
+                    if (!context.mounted) return;
+                    dismissIndicator(context);
+                    await showOkAlertDialog(
+                      context: context,
+                      title: 'ログインしました',
+                    );
+                    if (!context.mounted) return;
+                    Theme.of(context);
+                  } on Exception catch (e) {
+                    dismissIndicator(context);
+                    unawaited(
+                      showOkAlertDialog(
+                        context: context,
+                        title: 'エラー',
+                        message: e.toString(),
+                      ),
+                    );
+                  }
+                },
+              ),
               ElevatedButton(
                 child: const Text('匿名でログインする'),
                 onPressed: () async {
